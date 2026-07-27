61994119a69babde3f4a2fd16dcf5af00275d66f
Gautam Prasad Upadhyay <157909401+gautam-upadhyay@users.noreply.github.com>
2026-07-28 02:00:17 +0530

    Update 263_ugly_number.py - LeetCode Problem 263

--- 263_ugly_number.py
+++ 263_ugly_number.py
@@ -2,10 +2,13 @@ class Solution:
     def isUgly(self, n: int) -> bool:
         if n <= 0:
             return False
-        while n % 2 == 0:
-            n = n // 2
-        while n % 3 == 0:
-            n = n // 3
-        while n % 5 == 0:
-            n = n // 5
+        for i in [2,3,5]:
+            while n % i == 0:
+                n = n // i
+        # while n % 2 == 0:
+        #     n = n // 2
+        # while n % 3 == 0:
+        #     n = n // 3
+        # while n % 5 == 0:
+        #     n = n // 5
         return n == 1
