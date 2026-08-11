3e4b1b11a42f7e31f8a8dbf6ada7f00048342681
Gautam Prasad Upadhyay <157909401+gautam-upadhyay@users.noreply.github.com>
2026-08-11 23:49:41 +0530

    Add 451_sort_characters_by_frequency.py - LeetCode Problem 451

---
+++ 451_sort_characters_by_frequency.py
@@ -0,0 +1,11 @@
+from collections import Counter
+class Solution:
+    def frequencySort(self, s: str) -> str:
+        count = Counter(s)
+
+        result = []
+
+        for char, freq in count.most_common():
+            result.append(char * freq)
+        
+        return "".join(result)
