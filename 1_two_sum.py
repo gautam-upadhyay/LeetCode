240c0e39354021ff78bf1571a8aee945a1c45201
Gautam Prasad Upadhyay <157909401+gautam-upadhyay@users.noreply.github.com>
2026-08-21 23:58:35 +0530

    Update 1_two_sum.py - LeetCode Problem 1

--- 1_two_sum.py
+++ 1_two_sum.py
@@ -1,28 +1,19 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        d = {}
+
+        # for i in range(len(nums)):
+        #     for j in range(i+1, len(nums)):               #T.C O(n^2)
+        #         if (nums[i] + nums[j] == target):         #S.C O(1)
+        #             return [i,j]
+        
+
+        map = {}
 
         for i in range(len(nums)):
             require = target - nums[i]
+            if require in map:
+                return [map[require], i]
 
-            if require in d:
-                return (d[require], i)
-
-            d[nums[i]] = i
+            map[nums[i]] = i
 
         return []
-
-## can't do two pointer as it take o(nlogn)
-        # nums.sort()
-        # left = 0
-        # right = len(nums) - 1
-
-        # while left < right:
-        #     total = nums[left] + nums[right]
-        #     if total == target:
-        #         return [left , right]
-        #     elif total < target:
-        #         left += 1
-        #     else:
-        #         right -= 1
-        # return []
