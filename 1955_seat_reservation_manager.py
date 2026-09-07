3d55bd4cfe5098e7c1c51fd4b9975f956d9848d1
Gautam Prasad Upadhyay <157909401+gautam-upadhyay@users.noreply.github.com>
2026-09-07 23:56:25 +0530

    Update 1955_seat_reservation_manager.py - LeetCode Problem 1955

--- 1955_seat_reservation_manager.py
+++ 1955_seat_reservation_manager.py
@@ -5,15 +5,39 @@ class SeatManager:
     def __init__(self, n: int):
         self.available = list(range(1, (n+1)))
         heapq.heapify(self.available)
+        
 
     def reserve(self) -> int:
         return heapq.heappop(self.available)
-
         
+
     def unreserve(self, seatNumber: int) -> None:
         return heapq.heappush(self.available, seatNumber)
+
         
 
+
+# Your SeatManager object will be instantiated and called as such:
+# obj = SeatManager(n)
+# param_1 = obj.reserve()
+# obj.unreserve(seatNumber)
+
+
+# import heapq
+
+# class SeatManager:
+
+#     def __init__(self, n: int):
+#         self.available = list(range(1, (n+1)))
+#         heapq.heapify(self.available)
+
+#     def reserve(self) -> int:
+#         return heapq.heappop(self.available)
+
+        
+#     def unreserve(self, seatNumber: int) -> None:
+#         return heapq.heappush(self.available, seatNumber)
+        
 
 # Your SeatManager object will be instantiated and called as such:
 # obj = SeatManager(n)
